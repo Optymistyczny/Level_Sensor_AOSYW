146117743ca30b4ab609ace78852f2140c3d909f
Jakub <jb.stankiewicz12@gmail.com>
2024-12-02 13:12:41 +0100

    Refactor.

--- AOSYW_PROJ/src/SEN0311.c
+++ AOSYW_PROJ/src/SEN0311.c
@@ -7,34 +7,30 @@
 
 float SEN0311getFloatValue(sensor_t* sensor)
 {
-    status_t status;
-    uint8_t buff [EXPECTED_LEN]={};
+    uint8_t buff [EXPECTED_LEN]={0};
     float out = 0.0;
 
-    if(NULL==sensor)status = ERROR;
-
-    if(OK == status)
-    {
-        status_t status;
-        status=SEN0311_UART_Receive(buff, EXPECTED_LEN);
-        status=status | processInput(buff, EXPECTED_LEN, &out);
-    }
-
-    if(ERROR==status) out = 0.0;
+    if(NULL==sensor) return -1.0;
+    if(ERROR == SEN0311_UART_Receive(buff, EXPECTED_LEN)) return -1.0;
+    if(ERROR == processInput(buff, EXPECTED_LEN, &out)) return -1.0;
     return out;
+}
+
+static uint8_t calculateChecksum(const uint8_t* buff) {
+    return (buff[HEADER_BYTE] + buff[HBYTE] + buff[LBYTE]) & 0xFF;
 }
 
 status_t processInput(const uint8_t* buff, const uint8_t len, float* out)
 {
     status_t status=OK;
     uint16_t temp, sum;
-    *out = 0.0;
-    if(buff==NULL || len != EXPECTED_LEN) status = ERROR;
+    if (buff == NULL || out == NULL || len != EXPECTED_LEN) return ERROR;
 
-    sum=(buff[HEADER_BYTE]+buff[HBYTE]+buff[LBYTE]) & 0xFF; 
+    sum=calculateChecksum(buff); 
+
     if(buff[SUM_BYTE] != sum) status=ERROR;
 
-    printf("\nSum expected %x \t Calculated %x",buff[SUM_BYTE], ((buff[HEADER_BYTE]+buff[HBYTE]+buff[LBYTE]) & 0xFF));
+    // printf("\nSum expected %x \t Calculated %x",buff[SUM_BYTE], ((buff[HEADER_BYTE]+buff[HBYTE]+buff[LBYTE]) & 0xFF));
     if(OK==status)
     {   
         temp=(buff[HBYTE]*256+buff[LBYTE]);
