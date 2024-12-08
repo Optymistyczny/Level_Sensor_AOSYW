ab34c7892916cf9bd8a38244b98d1581499cae48
Jakub <jb.stankiewicz12@gmail.com>
2024-12-08 22:10:09 +0100

    Działa bez zarzutu. Print na konsolę. Teraz dodać FreeRTOSa.

--- AOSYW_PROJ/src/DS18B20.h
+++ AOSYW_PROJ/src/DS18B20.h
@@ -1,8 +1,10 @@
 #ifndef DS18B20_H
 #define DS18B20_H
 
-#include "SensorManagerData.h"
+#include "SensorManagerData.h"// for status_t
 
-float DS18B20getFloatValue(sensor_t* sensor);
-
+//Function signature must correspond to the interface to properly assign ptr
+status_t DS18B20_getTemp_Celcious(const sensor_t* const sensor, float* val);
+status_t DS18B20_getTemp_Fahrenheit(const sensor_t* const sensor, float* val);
+status_t DS18B20_getName(const sensor_t* const sensor, char* buffer, const uint8_t len);
 #endif
